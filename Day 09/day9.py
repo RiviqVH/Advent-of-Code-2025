99ebdba5af8c0b492b72099f45a1f827c624beb6
Vladimir Hazeleger <vladimir.hazeleger@riviq.nl>
2025-12-03 13:50:17 +0100

    initial commit

---
+++ Day 09/day9.py
@@ -0,0 +1,13 @@
+### Setup: ###
+import os
+
+os.chdir(r"C:\Users\VladimirHazeleger\Documents\Repositories\Advent of Code 2025\Day 9")
+
+### Read input: ###
+with open('input.txt') as f:
+    input = f.read()
+    input_by_line = [l.strip('\n') for l in f.readlines()]
+
+# print(input)
+
+### Part 1: ###
